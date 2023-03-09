bec0e8d95dbea21ed907977f342938b4680fb2ce
Yared Getachew <yaredsha@gmail.com>
2023-03-09 15:55:46 +0100

    tab bottom menu bar added

--- final/car-rent/App.js
+++ final/car-rent/App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createBottomTabNavigator();
+
+const Rent = ({ route }) => {
+  return (
+    <View>
+      <Text>Rent</Text>
+    </View>
+  );
+};
+
+const Bookings = ({ route }) => {
+  return (
+    <View>
+      <Text>Bookings</Text>
+    </View>
+  );
+};
+
+const Profile = ({ route }) => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Rent"
+      screenOptions={{
+        tabBarActiveTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Rent"
+        component={Rent}
+        options={{
+          tabBarLabel: "Rent",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="car-key" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Bookings"
+        component={Bookings}
+        options={{
+          tabBarLabel: "Bookings",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="clipboard-list-outline"
+              color={color}
+              size={size}
+            />
+          ),
+          tabBarBadge: 3,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
