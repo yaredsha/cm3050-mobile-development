d8482fa16ac1ab1494a8e7592db9b12e87978b1a
Yared Getachew <yaredsha@gmail.com>
2023-03-11 08:19:37 +0100

    profile saving and loading to secure store added

---
+++ final/car-rental/Profile.js
@@ -0,0 +1,24 @@
+import * as SecureStore from "expo-secure-store";
+
+const profileKey = "@profile_key";
+
+/**
+ *
+ * @param {Object} profile The profile object to save to secure store
+ */
+const saveProfile = async (profile) => {
+  return await SecureStore.setItemAsync(profileKey, JSON.stringify(profile));
+};
+
+/**
+ *
+ * @returns The profile from the secure store
+ */
+const loadProfile = async () => {
+  return await SecureStore.getItemAsync(profileKey);
+};
+
+export default {
+  saveProfile,
+  loadProfile,
+};
